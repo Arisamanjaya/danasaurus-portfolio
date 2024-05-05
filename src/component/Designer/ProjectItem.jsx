ac9f332d682b35986fe21be1c8d4dd013a21d659
Arisamanjaya <arisamanjaya09@gmail.com>
2024-05-05 23:35:30 +0700

    HeroSection Update Major

--- src/component/Designer/ProjectItem.jsx
+++ src/component/Designer/ProjectItem.jsx
@@ -10,12 +10,12 @@ const ProjectItem = ({ title, description }) => {
       <div className="py-4 grid justify-items-center lg:justify-items-start">
         <h3 className="text-lg font-semibold text-slate-800 mb-2 text-center lg:text-start">{title}</h3>
         <p className="text-sm font-normal text-slate-500 mb-4 text-center lg:text-start">{description}</p>
-        <div>
+        <a>
           <button className="bg-primary-500 text-white py-2 px-6 rounded-full text-xs font-normal flex items-center justify-center focus:bg-primary-400 hover:bg-primary-300 transition ease-in-out duration-500">
             View Detail
             <FiChevronRight className="ml-1" /> {/* Tambahkan ikon panah ke kiri */}
           </button>
-        </div>
+        </a>
       </div>
     </div> 
   );
--- src/component/Home/HomeSection.jsx
+++ src/component/Home/HomeSection.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-
+import { RiDownload2Line, RiMailFill } from 'react-icons/ri';
 
 
 const HomeSection = () => {
     return (
         <section className="max-w mx-auto h-screen px-6 lg:px-8 py-28 bg-primary-900 flex flex-col justify-center items-center gap-16">
-
+            <div className='max-w-sm gap-4 flex flex-col'>
+                <h1 className='flex flex-col text-center font-semibold text-white text-5xl'>
+                    <a href="/">
+                        Dana,
+                    </a>
+                    <a href="">
+                        Designer,
+                    </a>
+                    <a href="">
+                        Experience,
+                    </a>
+                    <a href="">
+                        Dansaurus
+                    </a>
+                </h1>
+                <p className='text-base font-normal text-white text-center'>
+                    Arisamanjaya is a designer based in Bali, Also Known as Danasaurus.
+                </p>
+                <div className='flex justify-center gap-5'>
+                    <a href="">
+                        <button className='bg-white rounded-full py-2 px-6 text-cyan-800 text-xs font-normal tracking-wide flex justify-center items-center focus:outline-none'>
+                            Contact Me
+                            < RiMailFill className='ml-1'/>
+                        </button>
+                    </a>
+                    <a href="">
+                        <button className='border border-white rounded-full py-2 px-6 text-white text-xs font-normal tracking-wide flex justify-center items-center focus:outline-none'>
+                            My Resume
+                            < RiDownload2Line className='ml-1'/>
+                        </button>
+                    </a>
+                </div>
+            </div>
         </section>
     );
 }
